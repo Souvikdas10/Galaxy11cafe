963c611d01c62bd407228822464db6b84b1b1fb4
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-07-12 19:28:23 +0530

    webp format added

--- middleware/mutiImg.js
+++ middleware/mutiImg.js
@@ -18,7 +18,8 @@ const uploadlmage = multer({
         if (
             file.mimetype == 'image/png' ||
             file.mimetype == 'image/jpg' ||
-            file.mimetype == 'image/jpeg'
+            file.mimetype == 'image/jpeg'||
+            file.mimetype == 'image/webp'
         ) {
             callback(null, true)
 
