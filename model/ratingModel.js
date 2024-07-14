a72dafd09eca8bd26e56bec9e031c0768fbff472
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-07-14 13:48:05 +0530

    rating model updated

--- model/ratingModel.js
+++ model/ratingModel.js
@@ -4,6 +4,7 @@ const path = require('path')
 const ratingSchema = new mongoose.Schema({
     name: {
         type: String,
+        required:true,
     },
     description: {
         type: String,
